7aaf41abda1d6e8659bea49352462729722f170f
rachel65 <rtomas65@gmail.com>
2015-02-27 11:36:26 -0700

    Whole Push Attempt

---
+++ Tomas, Rachel Portfolio/csi/nav.js
@@ -0,0 +1,9 @@
+document.write(
+'<div class="nav-item"><a href="otherSites.html">SITES</a></div>' +
+'<div class="nav-item"><a href="mitApp.html">MIT APP INVENTOR</a></div>' +
+'<div class="nav-item"><a href="aboutMe.html">ABOUT</a></div>' +
+'<div class="nav-item"><a href="photoshop.html">PHOTOSHOP</a></div>' +
+'<div class="nav-item"><a href="flash.html">FLASH</a></div>' +
+'<div class="nav-item"><a href="dreamweaver.html">DREAMWEAVER</a></div>' +
+'<div class="nav-item"><a href="portal.html">HOME</a></div>'
+);
