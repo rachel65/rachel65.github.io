bb64cd9abe79c082ea00f4cc51d7e33e6a758a02
rachel65 <rtomas65@gmail.com>
2015-03-02 07:55:17 -0700

    Internal File Pages Nav Support
    Javascript to define root folder and link nav through root

--- csi/nav.js
+++ csi/nav.js
@@ -1,9 +1,21 @@
+var loc = window.location.href;
+var idx;
+var rootFolder;
+
+if (idx === -1) {
+	idx = loc.lastIndexOf("/");
+} else {
+	idx = loc.lastIndexOf("/pages");
+};
+
+rootFolder = loc.substr(0, idx);
+
 document.write(
-'<div class="nav-item"><a href="otherSites.html">SITES</a></div>' +
-'<div class="nav-item"><a href="mitApp.html">MIT APP INVENTOR</a></div>' +
-'<div class="nav-item"><a href="aboutMe.html">ABOUT</a></div>' +
-'<div class="nav-item"><a href="photoshop.html">PHOTOSHOP</a></div>' +
-'<div class="nav-item"><a href="flash.html">FLASH</a></div>' +
-'<div class="nav-item"><a href="dreamweaver.html">DREAMWEAVER</a></div>' +
-'<div class="nav-item"><a href="portal.html">HOME</a></div>'
+'<div class="nav-item"><a href="' + rootFolder + '/otherSites.html">SITES</a></div>' +
+'<div class="nav-item"><a href="' + rootFolder + '/mitApp.html">MIT APP INVENTOR</a></div>' +
+'<div class="nav-item"><a href="' + rootFolder + '/aboutMe.html">ABOUT</a></div>' +
+'<div class="nav-item"><a href="' + rootFolder + '/photoshop.html">PHOTOSHOP</a></div>' +
+'<div class="nav-item"><a href="' + rootFolder + '/flash.html">FLASH</a></div>' +
+'<div class="nav-item"><a href="' + rootFolder + '/dreamweaver.html">DREAMWEAVER</a></div>' +
+'<div class="nav-item"><a href="' + rootFolder + '/portal.html">HOME</a></div>'
 );
